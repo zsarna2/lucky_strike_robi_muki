5ab26a67fbca19d801ec550a8a8d1028cfffa2d0
zsarna2 <zsarna2@gmail.com>
2018-01-24 14:02:33 +0100

    Results method start

---
+++ Result.java
@@ -0,0 +1,14 @@
+public class Result {
+
+    int number;
+    String color;
+    String evenodds;
+    String half;
+    String dozen;
+    String column;
+    Statistics resstats;
+
+    public Result(Statistics resstats) {
+        this.resstats = resstats;
+    }
+}
