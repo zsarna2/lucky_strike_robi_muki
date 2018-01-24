f6895f9074ef9cf8b2e250f77fa2d344ddcb79f8
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-01-24 13:11:55 +0100

    stats working with hashmap

--- Statistics.java
+++ Statistics.java
@@ -17,6 +17,7 @@ public class Statistics {
 
 
     public static void stats() {
+        HashMap<String, Integer> total = new HashMap<String, Integer>();
         int[] oppnumbers1 = new int[36];
         String[] oppzones = {"Zero", "Reds", "Blacks", "Evens", "Odds", "Frist dozen", "Second dozen", "Third dozen", "First half", "Second half", "Fist column", "Second column", "Third column"};
         for (int i = 1;i <= 36;i++) {
@@ -26,12 +27,16 @@ public class Statistics {
 
         Simulation text = Simulation.load();
         for (String item : text.content) {
-            int numcount = 0;
             if (Statistics.isInteger(item)) {
                 for (String item2 : oppnumbers2) {
                     if (item2.equals(item)) {
-                        numcount++;
-                        System.out.println(item2 + " " + numcount);
+                        if (!total.containsKey(item2)){
+                            total.put(item2, 1);
+                        } else {
+                            int temp = total.get(item2);
+                            temp++;
+                            total.put(item2, temp);
+                        }
                     }
                 }
             }
@@ -45,7 +50,15 @@ public class Statistics {
             int zonecount = 0;
             while (matcher.find())
                 zonecount++;
-            System.out.println(item + " " + zonecount);
+            if (item.equals("Zero")) {
+                zonecount = zonecount / 6;
+            }
+            total.put(item, zonecount);
+        }
+
+        for (String zone : total.keySet()) {
+            String value = total.get(zone).toString();
+            System.out.println(zone + ": " + value);
         }
     }
 }
