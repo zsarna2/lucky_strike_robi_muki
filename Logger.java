39ed2315d27cd29132af18d5238d46a9e776764e
nagyrobert1990 <nagy.robert1990@gmailcom>
2018-01-25 10:59:58 +0100

    the simulation is fully working

--- Logger.java
+++ Logger.java
@@ -7,6 +7,7 @@ public class Logger {
     public void log(String type, String message){
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss").format(new Timestamp(System.currentTimeMillis()));
 
-        System.out.println(timeStamp + ": " + type + " | " + message);
+        System.out.printf("%s: %-32s |%-8s\n", timeStamp, type, message);
+        System.out.println("-------------------------------------------------------------------------------");
     }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -14,15 +17,30 @@ public class Main {
     public static void main(String[] args) {
 
         Logger logger = new Logger();
-        if (Integer.parseInt(args[0]) <= 0) {
-            logger.log("error", "illegal round number given");
-        } else {
-        
-            Simulation test = Main.generateSimulation(Integer.parseInt(args[0]));
-            Simulator simulator = new Simulator(test, logger);
-            Result result = simulator.run();
+        try {
+            if (Integer.parseInt(args[0]) <= 0) {
+                logger.log("error", "illegal round number given");
+            } else {
+            
+                Simulation test = Main.generateSimulation(Integer.parseInt(args[0]));
+                Simulator simulator = new Simulator(test, logger);
+                Result result = simulator.run();
 
-            System.out.println(result.column);
+                Map<String, Integer> treeMap = new TreeMap<String, Integer>(result.resstats.stats);
+                for (Map.Entry<String, Integer> entry : treeMap.entrySet()){
+                    String name = entry.getKey();
+                    String value = entry.getValue().toString();  
+                    logger.log(name,value);
+                }
+                logger.log("the numbers you win with",result.number);
+                logger.log("the color",result.color);
+                logger.log("the evenness",result.evenodd);
+                logger.log("the dozen",result.dozen);
+                logger.log("the half",result.half);
+                logger.log("the column",result.column);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.log("error","type how many rounds you want to simulate");
         }
     }
 }
